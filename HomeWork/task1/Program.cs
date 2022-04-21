72607a77ef96b3871ccc573cda8946473b669120
GreIS93 <grekovis93@gmail.com>
2022-04-21 22:55:35 +0700

    hwtask1

--- HomeWork/task1/Program.cs
+++ HomeWork/task1/Program.cs
@@ -1,1 +1,64 @@
 ﻿// Написать программу, которая в двумерном массиве заменяет строки на столбцы или сообщить, что это невозможно (в случае, если матрица не квадратная).
+
+void FillArray(int[,] matr1, int first, int last)
+{
+    for (int i = 0; i < matr1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr1.GetLength(1); j++)
+        {
+            matr1[i, j] = new Random().Next(first, last);
+        }
+    }
+}
+
+void PrintArray(int[,] matr2)
+{
+    for (int i = 0; i < matr2.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr2.GetLength(1); j++)
+        {
+            Console.Write($"{matr2[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int[,] matrix = new int[rows, columns];
+
+// int RowToColumn(int [,] matr3)
+// {
+//     {
+//         for (int i=0; i<matr3.GetLength(0); i++)
+//         {
+//             for (int j= 0; j<matr3.GetLength(1); j++)
+//             {
+//                 matr3[i,j] = matr3[j,i];
+//             }
+//         }
+//     }
+//     return matr3;
+// }
+
+if (rows == columns)
+{
+    FillArray(matrix, 0, 10);
+    PrintArray(matrix);
+    Console.WriteLine();
+
+    int[,] transposeMatrix = new int[matrix.GetLength(1), matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            transposeMatrix[j, i] = matrix[i, j];
+        }
+    }
+    PrintArray(transposeMatrix);
+}
+else Console.WriteLine("Заменить строки на столбцы невозможно");
--- HomeWork/task2/Program.cs
+++ HomeWork/task2/Program.cs
@@ -1,1 +1,62 @@
 ﻿// Найти произведение двух матриц
+
+void FillArray(int[,] matr1)
+{
+    for (int i = 0; i < matr1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr1.GetLength(1); j++)
+        {
+            matr1[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matr2)
+{
+    for (int i = 0; i < matr2.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr2.GetLength(1); j++)
+        {
+            Console.Write($"{matr2[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк");
+int k = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int t = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int[,] matrix = new int[k, t];
+
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+
+int[,] matrix2 = new int[k, t];
+
+FillArray(matrix2);
+PrintArray(matrix2);
+Console.WriteLine();
+
+int[,] ProductArrays(int[,] matr1,int[,] matr2)
+{
+    int[,] matr3 = new int[matr1.Length, matr2.Length];
+    for (int i = 0; i < matr2.GetLength(1); i++)
+    {
+        for (int j = 0; j < matr2.GetLength(0); j++)
+        {
+            matr3 [i, j] = 0;
+                    for (int k = 0; k < matr2.GetLength(0); k++)
+                    {
+                        matr3[i, j] = matr1[i, k] * matr2[k, j];
+                    }
+        }
+    }
+    return matr3;
+}
+
+int[,] matrix3 = ProductArrays(matrix,matrix2);
+PrintArray(matrix3);
